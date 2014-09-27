4b6ad19b6746c80d32448952ac0051fea1e159b0
Dulio Denis <dulio.denis@yahoo.com>
2014-09-27 07:41:15 -0400

    Hid Status bar

--- drizzle/drizzle/AppDelegate.swift
+++ drizzle/drizzle/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
-        // Override point for customization after application launch.
+        application.setStatusBarHidden(true, withAnimation:.None)
         return true
     }
 
