8ce8cd49844b0a461ff70c7b7b39f71dc447b7de
Dulio Denis <dulio.denis@yahoo.com>
2014-09-29 07:11:21 -0400

    Added Launch Screen

--- drizzle/drizzle/ViewController.swift
+++ drizzle/drizzle/ViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // MARK: Weather Properties
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var precipitationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
+    var coordinates = "40.7606,-73.984" // Initial Coordinates
     
     private let apiKey = "YOUR-FORECAST-API-KEY"
-                            
+    
+    // MARK: View Hierarchy
     override func viewDidLoad() {
         super.viewDidLoad()
-        let coordinates = "40.7606,-73.984" // Initial Coordinates
+        
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+    }
+
+    
+    // MARK: Fetch Weather Data Methods
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forecastURL = NSURL(string: coordinates, relativeToURL: baseURL)
         let weatherData = NSData.dataWithContentsOfURL(forecastURL, options: nil, error: nil)
@@ -45,12 +57,44 @@ class ViewController: UIViewController {
                     self.humidityLabel.text = "\(currentWeatherDictionary.humidity)"
                     self.precipitationLabel.text = "\(currentWeatherDictionary.precipitationProbability)"
                     self.summaryLabel.text = "\(currentWeatherDictionary.summary)"
+                    
+                    // stop the activity indicator animation and put back the refresh button
+                    self.stopActivityIndicator()
+                })
+                
+            } else {
+                let networkIssueController = UIAlertController(title: "Error",
+                    message: "Unable to load data due to a connectivity error.",
+                    preferredStyle: .Alert)
+                let dismissButton = UIAlertAction(title: "Dismiss", style: .Default, handler: nil)
+                networkIssueController.addAction(dismissButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.stopActivityIndicator()
                 })
                 
             }
             
         })
         downloadTask.resume()
+    }
+    
+    
+    @IBAction func refresh() {
+        getCurrentWeatherData()
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+    }
+    
+    
+    func stopActivityIndicator() -> Void {
+        // stop the activity indicator animation and put back the refresh button
+        self.refreshButton.hidden = false
+        self.refreshActivityIndicator.stopAnimating()
+        self.refreshActivityIndicator.hidden = true
     }
 
 }
