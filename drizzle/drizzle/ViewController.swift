2c74936aa02762102a2255d6463b6063e66d5529
Dulio Denis <dulio.denis@yahoo.com>
2014-10-03 21:40:02 -0400

    Added core location functionality

--- drizzle/drizzle/ViewController.swift
+++ drizzle/drizzle/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: Weather Properties
     @IBOutlet weak var iconView: UIImageView!
@@ -18,8 +19,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var precipitationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
+    var locationManager = CLLocationManager()
+    var locationReceived = false
     var coordinates = "40.7606,-73.984" // Initial Coordinates
     
     private let apiKey = "YOUR-FORECAST-API-KEY"
@@ -27,6 +31,11 @@ class ViewController: UIViewController {
     // MARK: View Hierarchy
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
         
         refreshActivityIndicator.hidden = true
         getCurrentWeatherData()
@@ -44,10 +53,28 @@ class ViewController: UIViewController {
         let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
             
             if (error == nil) {
+                var jsonError: NSError?
                 let dataObject = NSData(contentsOfURL: location)
-                let weatherDictionary : NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
+                let json : AnyObject! = NSJSONSerialization.JSONObjectWithData(dataObject, options: NSJSONReadingOptions.MutableContainers, error: &jsonError)
+                if let err = jsonError {
+                    
+                    let jsonIssueController = UIAlertController(title: "Sorry Dude",
+                        message: "Unable to acquire weather data due to a weather server issue.",
+                        preferredStyle: .Alert)
+                    let dismissButton = UIAlertAction(title: "Dismiss", style: .Default, handler: nil)
+                    jsonIssueController.addAction(dismissButton)
+                    
+                    self.presentViewController(jsonIssueController, animated: true, completion: nil)
+                    
+                    return
+                }
+                
+                var weatherDictionary = json as NSDictionary
+                
+               // let weatherDictionary : NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
                 
                 let currentWeatherDictionary = Current(weatherDictionary: weatherDictionary)
+                self.locationManager.startUpdatingLocation()
                 
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     self.temperatureLabel.text = "\(currentWeatherDictionary.temperature)"
@@ -63,8 +90,8 @@ class ViewController: UIViewController {
                 })
                 
             } else {
-                let networkIssueController = UIAlertController(title: "Error",
-                    message: "Unable to load data due to a connectivity error.",
+                let networkIssueController = UIAlertController(title: "Sorry but",
+                    message: "Unable to load data due to a connectivity error. Try later, dude.",
                     preferredStyle: .Alert)
                 let dismissButton = UIAlertAction(title: "Dismiss", style: .Default, handler: nil)
                 networkIssueController.addAction(dismissButton)
@@ -84,6 +111,7 @@ class ViewController: UIViewController {
     
     @IBAction func refresh() {
         getCurrentWeatherData()
+        locationReceived = false
         refreshButton.hidden = true
         refreshActivityIndicator.hidden = false
         refreshActivityIndicator.startAnimating()
@@ -95,7 +123,38 @@ class ViewController: UIViewController {
         self.refreshButton.hidden = false
         self.refreshActivityIndicator.stopAnimating()
         self.refreshActivityIndicator.hidden = true
+        if (locationReceived) {
+            self.locationManager.stopUpdatingLocation()
+        }
     }
+    
+    // MARK: Location Manager Delegate Functions
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+       println("An error occurred: \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        coordinates = "\(locValue.latitude),\(locValue.longitude)"
+        locationReceived = true
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil) {
+                return
+            }
+            
+            if placemarks.count > 0 {
+                self.cityLabel.text = placemarks[0].locality
+            }
+        })
+    
+        getCurrentWeatherData()
+    }
+    
+    
+    
 
 }
 
